22f53fda69809ed85408e59bf763c4be654809cb
Michael Pedersen <michaelp@discoverdst.com>
2021-08-05 02:25:34 -0500

    Working minimal viable plugin

--- main.ts
+++ main.ts
@@ -1,38 +1,33 @@
 import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import fetch from "node-fetch";
 
 interface MyPluginSettings {
-	mySetting: string;
+	autoArchiveOnDocumentLoad: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	autoArchiveOnDocumentLoad: false
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
-
+	
 	async onload() {
-		console.log('loading plugin');
-
+		console.log("Loading Link Archiver");
 		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
+		// this.addRibbonIcon('dice', 'Sample Plugin', () => {
+		// 	new Notice('This is a notice!');
+		// });
 
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
+			id: 'auto-link-archiver-save-open-page',
+			name: 'Archive all links on current page',
 			checkCallback: (checking: boolean) => {
 				let leaf = this.app.workspace.activeLeaf;
 				if (leaf) {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new SampleModal(this.app, this.cm).open();
 					}
 					return true;
 				}
@@ -40,21 +35,35 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
 			console.log('codemirror', cm);
+			this.cm = cm;
+			
+			cm.on("cursorActivity", (cm: CodeMirror.Editor) => {
+				const current = cm.getCursor().line + 1;
+				if (cm.state.curLineNum === current) {
+					return;
+				}
+				// console.log(current);
+				// console.log("doc", cm.getDoc().getValue());
+			});
+
+			this.registerEvent(
+				this.app.metadataCache.on("changed", (file) => {
+					// file saves, etc.
+					console.log('metadata cache changed', file);
+				})
+			  );
 		});
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// const a = document.getElementsByClassName("CodeMirror");
+		// console.log("a", a);		
 	}
 
 	onunload() {
-		console.log('unloading plugin');
+		console.log('unloading Link Archiver plugin');
 	}
 
 	async loadSettings() {
@@ -67,13 +76,43 @@ export default class MyPlugin extends Plugin {
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
+	constructor(app: App, cm: CodeMirror.Editor) {
 		super(app);
+		this.cm = cm;
+		this.modalContent = "";
 	}
 
 	onOpen() {
 		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		let urlStatuses = [];
+
+		const currentNoteContents = this.cm.getDoc().getValue();
+		const regex = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/g
+		const urlMatches = currentNoteContents.match(regex) || [];
+		const matchCount = urlMatches.length;
+		if (matchCount === 0){
+			return;
+		}
+		const header = (index) => `<h1>Link Archiving Status (${index}/${matchCount})</h1>`
+		let i = 0;
+		contentEl.innerHTML = header(i);
+
+		urlMatches.forEach(async (url) => {
+			const result = await fetch(`https://web.archive.org/save/${url}`)
+			const status = {
+				sourceUrl: url,
+				url: result.url,
+				status: result.status,
+				ok: result.ok
+			}
+			urlStatuses.push(status);
+			contentEl.innerHTML = `
+				${header(++i)}
+				<ul>
+					${urlStatuses.map(result => `<li>${result.ok ? '✓' :  '✖' } <a href="${result.sourceUrl}">${result.sourceUrl}</a> => <a href="${result.url}">${result.url}</a></li>`).join('')}
+				</ul>
+			`
+		});
 	}
 
 	onClose() {
@@ -95,18 +134,18 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Link Archiver'});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		containerEl.createEl('h4', {text: 'Currently None :)'});
+
+		// new Setting(containerEl)
+		// 	.setName('Auto Archive on document open')
+		// 	.setDesc('Auto archive links anytime you open a document. We\'d rather not overload our friends at archive.org (They run the WaybackMachine).')
+		// 	.addToggle(toggle => toggle
+		// 		.onChange(async (value) => {
+		// 			console.log('Secret: ' + value);
+		// 			this.plugin.settings.autoArchiveOnDocumentLoad = value;
+		// 			await this.plugin.saveSettings();
+		// 		}));
 	}
 }
